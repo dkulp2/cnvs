ff5f38aa491ab03cb833eabe3de3ad69d001fdcb
David Kulp <dkulp@dizz.org>
2016-11-15 09:28:51 -0500

    New file locations

--- ui/viewCNV/config.R
+++ ui/viewCNV/config.R
@@ -6,12 +6,16 @@ extent.offset <- 12*100/2 # FIXME: outputs should set map positions centered in 
 extent.offfset <- 0
 
 profile.fn <- "d:/mccarroll/gpc_wave2_batch1/profile_seq_20_100.dat.gz"
-profile.fn <- '/home/dkulp/mccarroll/gpc_wave2_batch1/profile_seq_20_100.dat.gz'
+profile.fn <- '/home/dkulp/data/gpc_wave2_batch1/profile_seq_20_100.dat.gz'
+#profile.fn <- '/home/dkulp/mccarroll/gpc_wave2_batch1/unmasked/profile_seq_20_100.dat.gz'
 
 data.dir <- "d:/mccarroll/cnv_seg.12.500"
-data.dir <- "d:/mccarroll/cnv_seg.B12.L500.Q13"
+data.dir <- "C:/cygwin64/home/dkulp/data/out/cnv_seg.B12.L500.Q13.4"
+#data.dir <- '/home/dkulp/data/out/cnv_seg.B12.L500.Q13.4'
 
 tmp.dir <- "C:\\cygwin64\\tmp"
 
-input.dump.fn <- "d:/mccarroll/tmp/inputdump.R"
+input.dump.fn <- 'C:\\cygwin64\\home\\dkulp\\data\\tmp\\inputdump.R'
+
+db.conn.str <- 'dkulp:localhost:5432:seq'
 
