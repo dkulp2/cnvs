c5764dee8f1bc45fdf0e2fd61db3c9f2a94fa412
David Kulp <dkulp@dizz.org>
2017-01-06 12:22:41 -0500

    More

--- prep/prep.sh
+++ prep/prep.sh
@@ -17,9 +17,15 @@ ROLLING_WINDOWS=${SCRIPTS}/choose_windows.awk
 
 eval "gunzip -c ${profileFile}" | awk -v OFS="\t" -v NBINS=${NBINS} -v MAXLEN=${MAXLEN} -f ${ROLLING_WINDOWS} > ${ROOT}/sites.txt
 
+# generate simplified tab files from VCF
 zcat ${gsdelFile} | gs_del2tab -s ${ROOT}/sites.txt > ${gsdelFile%%.vcf.gz}.txt
 zcat ${gscnvFile} | gs_del2tab -s ${ROOT}/sites.txt > ${gscnvFile%%.vcf.gz}.txt
 
 # check source. Uses environment variables.
+# Generate _flt, _xflt and _best sets from samplesegs
 Rscript overlap.R
+
+# Load database with profiles
+perl profile2db.pl -i ${profileFile} -s ${profileFile%%.dat.gz}.seg.txt.gz -p ${profileFile%%.dat.gz}.pro.txt.gz
+sql profile2db.sql
 
