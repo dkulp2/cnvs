bc08a810287eb01e0aa7ecbd5468bea21fca9ddb
David Kulp <dkulp@dizz.org>
2017-01-13 10:05:00 -0500

    ROOT => workDir

--- prep/prep.sh
+++ prep/prep.sh
@@ -15,15 +15,22 @@ source ${THISDIR}/../conf/cnv.conf
 SCRIPTS=${THISDIR}/../pl
 ROLLING_WINDOWS=${SCRIPTS}/choose_windows.awk
 
-eval "gunzip -c ${profileFile}" | awk -v OFS="\t" -v NBINS=${NBINS} -v MAXLEN=${MAXLEN} -f ${ROLLING_WINDOWS} > ${ROOT}/sites.txt
+eval "gunzip -c ${profileFile}" | awk -v OFS="\t" -v NBINS=${NBINS} -v MAXLEN=${MAXLEN} -f ${ROLLING_WINDOWS} > ${workDir}/sites.txt
 
 # generate simplified tab files from VCF
-zcat ${gsdelFile} | gs_del2tab -s ${ROOT}/sites.txt > ${gsdelFile%%.vcf.gz}.txt
-zcat ${gscnvFile} | gs_del2tab -s ${ROOT}/sites.txt > ${gscnvFile%%.vcf.gz}.txt
+if [ -f ${gsdelFile} ]; then
+    zcat ${gsdelFile} | gs_del2tab -s ${workDir}/sites.txt > ${gsdelFile%%.vcf.gz}.txt
+fi
+
+if [ -f ${gscnvFile} ]; then
+    zcat ${gscnvFile} | gs_del2tab -s ${workDir}/sites.txt > ${gscnvFile%%.vcf.gz}.txt
+fi
 
 # check source. Uses environment variables.
 # Generate _flt, _xflt and _best sets from samplesegs
-Rscript overlap.R
+if [ -f ${gsdelFile} -a -f ${gscnvFile} ]; then
+    Rscript overlap.R
+fi
 
 # Load database with profiles
 perl profile2db.pl -i ${profileFile} -s ${profileFile%%.dat.gz}.seg.txt.gz -p ${profileFile%%.dat.gz}.pro.txt.gz
