538333cbe9cc3ea4debaf476f49443440f8a15ab
David Kulp <dkulp@dizz.org>
2017-03-12 00:15:56 -0500

    forgot to remove debug exit

--- ABCD.sh
+++ ABCD.sh
@@ -4,7 +4,7 @@
 # most recent source code tag
 export TAG=`git tag -l | tail -1`
 echo Using tag: ${TAG}
-exit 1
+
 for Q in A B C D; do
   export QUARTET_DIR=/humgen/cnp04/bobh/projects/segmentation/sfari/data_sfari_batch1${Q}
   time sh prep/prep.sh
