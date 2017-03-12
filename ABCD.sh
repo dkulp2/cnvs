84a543290bc7b4a0b78455ed7edda6d761b3ddac
David Kulp <dkulp@dizz.org>
2017-03-11 23:11:02 -0500

    quartet wrapper

---
+++ ABCD.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# run a test on a quartet set and set output to use most recent git tag
+
+# most recent source code tag
+export TAG=`git tag -l | head -1`
+echo Using tag: ${TAG}
+exit 1
+for Q in A B C D; do
+  export QUARTET_DIR=/humgen/cnp04/bobh/projects/segmentation/sfari/data_sfari_batch1${Q}
+  time sh prep/prep.sh
+  time sh pl/cnv_seg.sh
+done
