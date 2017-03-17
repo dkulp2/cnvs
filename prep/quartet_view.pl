c5f1e0ec52615394df5f667c76686de2ec87fc99
David Kulp <dkulp@dizz.org>
2017-03-17 14:55:32 -0400

    Update tables. Use UNION ALL

--- prep/quartet_view.pl
+++ prep/quartet_view.pl
@@ -5,10 +5,10 @@
 use strict;
 
 my $base="data_sfari_batch1";
-my @tables=qw(bkpt cnvs pois posterior posterior_dist prior prior_region profile_counts profile_segment);
+my @tables=qw(bkpt cnv_mle cnv_mrg cnv_post pois posterior posterior_dist prior prior_region profile_counts profile_segment);
 my @families=qw(a b c d);
 foreach my $T (@tables) {
     print "CREATE VIEW $base.$T AS\n";
-    print(join("UNION\n", map { "SELECT * FROM $base$_.$T\n" } @families),";\n");
+    print(join("UNION ALL\n", map { "SELECT * FROM $base$_.$T\n" } @families),";\n");
 }
 
