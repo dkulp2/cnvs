929057fc465e5624fa1dbeee2e4a69dda3f5b56f
David Kulp <dkulp@dizz.org>
2017-05-04 15:47:59 -0400

    Trivial comment

--- cleanup.sh
+++ cleanup.sh
@@ -4,7 +4,7 @@ rm data_sfari_batch1[ABCD]*/*/*smlcsm*
 # collapse
 rm data_sfari_batch1[ABCD]*/*/*smlx2csm*
 
-# priors
+# priors - SET ENVIRONMENT!!
 for i in data_sfari_batch1[ABCD]*; do
   psql -a -c "drop table ${i}.prior cascade; drop table ${i}.prior_region cascade"
 done
