992b1390093aef156b43f27600c952edcb67ac60
David Kulp <dkulp@dizz.org>
2017-05-07 23:47:59 -0400

    more general naming scheme

--- prep/quartet_view.pl
+++ prep/quartet_view.pl
@@ -5,14 +5,20 @@
 use strict;
 
 my $base="data_sfari_batch1";
+my $ext="_27apr2017";
+
+print "CREATE SCHEMA ${base};\n";
+
 my @tables=qw(bkpt cnv_mle cnv_mrg cnv_post pois posterior posterior_dist prior prior_region profile_counts geno);
 my @families=qw(a b c d);
+
 foreach my $T (@tables) {
-    print "DROP VIEW $base.$T;\n";
+    print "DROP VIEW IF EXISTS $base.$T;\n";
     print "CREATE VIEW $base.$T AS\n";
-    print(join("UNION ALL\n", map { "SELECT * FROM $base$_.$T\n" } @families),";\n");
+    print(join("UNION ALL\n", map { "SELECT * FROM $base$_$ext.$T\n" } @families),";\n");
 }
-print "DROP VIEW $base.profile_segment;\n";
-print "CREATE VIEW $base.profile_segment AS SELECT * FROM ${base}a.profile_segment;\n";
+
+print "DROP VIEW IF EXISTS $base.profile_segment;\n";
+print "CREATE VIEW $base.profile_segment AS SELECT * FROM ${base}a$ext.profile_segment;\n";
 
 
